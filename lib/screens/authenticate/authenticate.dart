7ff2f9c0ce6e6dd92a9c5537178439a6f139655a
mohamedafify <mohamed@afify.dev>
2021-03-02 19:54:00 +0200

    [feat]
    -created buttons to navigate through in authentication page
    -created new constant file to use through all the app

--- lib/screens/authenticate/authenticate.dart
+++ lib/screens/authenticate/authenticate.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:my_property/screens/authenticate/sign_in.dart';
+import 'package:my_property/screens/authenticate/register.dart';
+import 'package:my_property/screens/home/home.dart';
+import 'package:my_property/screens/utils/constant.dart';
 
 class Authenticate extends StatefulWidget {
   @override
@@ -9,8 +12,42 @@ class Authenticate extends StatefulWidget {
 class _AuthenticateState extends State<Authenticate> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-        child: SignIn(),
-    );
+	return Container(
+		color: Constant.backgroundColor,
+		padding: EdgeInsets.symmetric(vertical: 100.0, horizontal: 40.0),
+		child: Form(
+			child: Column(
+				children:<Widget> [
+					FlatButton(
+						child: Text("SignUp"),
+						onPressed: () {
+							Navigator.push(
+							context,
+							MaterialPageRoute(builder: (context) => Register()),
+							);
+						},
+					),
+					FlatButton(
+						child: Text("SignIn"),
+						onPressed: () {
+							Navigator.push(
+							context,
+							MaterialPageRoute(builder: (context) => SignIn()),
+							);
+						},
+					),
+					FlatButton(
+						child: Text("Guest"),
+						onPressed: () {
+							Navigator.push(
+							context,
+							MaterialPageRoute(builder: (context) => Home()),
+							);
+						},
+					),
+				],
+			),
+		),
+	);
   }
 }
---
+++ lib/screens/utils/constant.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class Constant {
+	static Color buttonTextColor = Colors.blue[900];
+	static Color backgroundColor = Colors.blue[300];
+}
